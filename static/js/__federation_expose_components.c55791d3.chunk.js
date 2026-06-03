27c2cf9bf4fecfbc653179891d13c6f2eb042f2f
zhipenglin <zhipenglin@users.noreply.github.com>
2026-06-03 10:21:02 +0000

    deploy: 044868897a4b9aaab119cb35c389c9d58cbd8e49

---
+++ static/js/__federation_expose_components.c55791d3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_react_filter_1_0_0=self.webpackChunk_react_filter_1_0_0||[]).push([[924],{8151(e,r,t){t.r(r),t.d(r,{default:()=>a,manifest:()=>i});const a={ReactFilter:t(58872).A},i={name:"react-filter",version:"1.0.0","open-version":!0,"public-url":"/react-filter",modules:[{name:"react-filter",baseDir:"/home/runner/work/react-filter/react-filter",description:"A React filter component library",packageName:"@kne/react-filter"}]}}}]);
+//# sourceMappingURL=__federation_expose_components.c55791d3.chunk.js.map
